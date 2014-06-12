c10c56684f95a614465612b0e4e5ceed02480670
Steffen Bruchmann <hi@sbruchmann.me>
2014-06-12 08:43:32 +0200

    Move HTTP server logic to `server/HTTPServer.js`
    This commit moves all code related to configuring, launching and closing
    of the HTTP server into its own module.
    
    Reasons for this change:
      - seperation of concerns
      - keeping code DRY
      - keeping code maintainable

---
+++ server/HTTPServer.js
@@ -0,0 +1,74 @@
+"use strict";
+
+var express = require("express");
+var http = require("http");
+var serveIndex = require("serve-index");
+var serveStatic = require("serve-static");
+
+function HTTPServer() {
+    this._socketPool = [];
+    this._server = null;
+    this.app = null;
+
+    return this;
+}
+
+HTTPServer.prototype._handleConnection = function _handleConnection(socket) {
+    var _socketPool = this._socketPool;
+
+    _socketPool.push(socket);
+    socket.on("close", function _handleClose() {
+        _socketPool.splice(_socketPool.indexOf(socket), 1);
+    });
+
+    return this;
+};
+
+HTTPServer.prototype._setup = function _setup(options) {
+    var app = express();
+    var basepath = options.basepath;
+
+    app
+        .use(serveStatic(basepath))
+        .use(serveIndex(basepath));
+
+    this._server = http.createServer(app);
+    this.app = app;
+
+    return this;
+};
+
+HTTPServer.prototype.close = function close(done) {
+    var self = this;
+
+    this._socketPool.forEach(function _iterate(socket) {
+        socket.destroy();
+    });
+    this._server.close(function _callback(err) {
+        if (err) {
+            return done(err);
+        }
+
+        self._server = null;
+        self._socketPool = [];
+        self.app = null;
+
+        done(null);
+    });
+
+    return this;
+};
+
+/**
+ * @TODO Add general error handling (EACCES, EADDRINUSE, etc.)
+ */
+HTTPServer.prototype.launch = function launch(options, done) {
+    this._setup(options);
+    this._server
+        .on("connection", this._handleConnection.bind(this))
+        .listen(options.port, options.hostname, done);
+
+    return this;
+};
+
+module.exports = HTTPServer;
--- server/ServerDomain.js
+++ server/ServerDomain.js
@@ -2,66 +2,22 @@
 "use strict";
 
 var _DomainConfig = require("./DomainConfig.json");
-var express = require("express");
-var http = require("http");
+var HTTPServer = require("./HTTPServer");
 
 var _commands = _DomainConfig.commands;
 var DOMAIN_ID = _DomainConfig.id;
 
-var app = null;
 var domainManager = null;
-var server = null;
+var server = new HTTPServer();
 
 var nextTick = process.nextTick;
 
-// Keep track of all connected sockets
-var _connectedSockets = [];
-
-// Keeps track of all connected sockets
-// and closes them if needed
-function _handleConnection(socket) {
-    _connectedSockets.push(socket);
-    socket.on('close', function _onSocketClose() {
-        _connectedSockets.splice(_connectedSockets.indexOf(socket), 1);
-    });
-}
-
 function closeServer(done) {
-    if (!server) {
-        nextTick(done.bind(null, null));
-    }
-
-    // In order to shutdown a HTTP server,
-    // one has to destroy all connected sockets manually
-    _connectedSockets.forEach(function _iterate(socket) {
-        socket.destroy();
-    });
-
-    server.close(function callback(err) {
-        if (err) {
-            return done(err);
-        }
-
-        app = null;
-        server = null;
-        done(null);
-    });
+    server.close(done);
 }
 
 function launchServer(options, done) {
-    var basepath = options.basepath;
-
-    app = express()
-        .use(require("serve-static")(basepath))
-        .use(require("serve-index")(basepath));
-
-    if (!server) {
-        server = http.createServer(app);
-    }
-
-    server
-        .on("connection", _handleConnection)
-        .listen(options.port, done);
+    server.launch(options, done);
 }
 
 exports.init = function init(manager) {
