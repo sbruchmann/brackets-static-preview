7e0ff56b5d9afd6ce9b0c5925e86a04010c0b146
Denisov21 <student@alice.it>
2014-06-27 17:05:22 +0200

    [NLS/IT] Fix missing translation (Closes #15)

--- nls/it/strings.js
+++ nls/it/strings.js
@@ -3,7 +3,7 @@ define({
     "CMD_STATIC_PREVIEW_SETTINGS": "Impostazioni Anteprima Statica\u2026",
     "RESTART" : "Riavvia Server",
     "DIALOG_RESTART_TITLE" : "Applica impostazioni",
-    "DIALOG_RESTART_TEXT" : "Per applicare le impostazioni modificate, Static Preview sarà necessario riavviare.",
+    "DIALOG_RESTART_TEXT" : "Per applicare le impostazioni modificate, Anteprima Statica sarà necessario riavviare.",
     "DIALOG_SETTINGS_TITLE" : "Impostazioni Anteprima Statica",
     "SETTING_HOSTNAME" : "Hostname",
     "SETTING_PORT" : "Porta"
