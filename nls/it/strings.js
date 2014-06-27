b228631d3909b1c6d00dbf0030d1cc9196ba8bbc
Denisov21 <student@alice.it>
2014-06-27 17:05:22 +0200

    Update strings.js

--- nls/it/strings.js
+++ nls/it/strings.js
@@ -1,9 +1,9 @@
 define({
     "CMD_STATIC_PREVIEW": "Anteprima Statica",
-    "CMD_STATIC_PREVIEW_SETTINGS": "Impostazioni Anteprima Statica\u2026",
+    "CMD_STATIC_PREVIEW_SETTINGS": "Impostazioni Anteprima Statica...\u2026",
     "RESTART" : "Riavvia Server",
     "DIALOG_RESTART_TITLE" : "Applica impostazioni",
-    "DIALOG_RESTART_TEXT" : "Per applicare le impostazioni modificate, Static Preview sarà necessario riavviare.",
+    "DIALOG_RESTART_TEXT" : "Per applicare le impostazioni modificate, Anteprima Statica sarà necessario riavviare.",
     "DIALOG_SETTINGS_TITLE" : "Impostazioni Anteprima Statica",
     "SETTING_HOSTNAME" : "Hostname",
     "SETTING_PORT" : "Porta"
