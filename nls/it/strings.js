473a82de7d83932a764b09031b2a86a19c343e16
Denisov21 <student@alice.it>
2014-06-27 10:20:23 +0200

    Create strings.js
    Add Italian Language!

---
+++ nls/it/strings.js
@@ -0,0 +1,8 @@
+define({
+    "RESTART": "Riavvia Server",
+    "DIALOG_RESTART_TITLE": "Applica impostazioni",
+    "DIALOG_RESTART_TEXT": "Per applicare le impostazioni modificate, Static Preview sarà necessario riavviare.",
+    "DIALOG_SETTINGS_TITLE": "Impostazioni Static Preview",
+    "SETTING_HOSTNAME": "Hostname",
+    "SETTING_PORT": "Porta"
+});
