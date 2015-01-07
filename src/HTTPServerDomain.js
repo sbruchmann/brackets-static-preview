e0cc54e68a05487b184327be834db4c2b5934b76
Steffen Bruchmann <steffen.bruchmann@gmail.com>
2015-01-07 21:20:29 +0100

    Initial implementation of HTTPServerDomain

---
+++ src/HTTPServerDomain.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * @const
+ * @type {string}
+ * @private
+ */
+var DOMAIN_ID = "sbruchmann.staticpreview.HTTPServer";
+
+function init(domainManager) {
+    if (!domainManager.hasDomain(DOMAIN_ID)) {
+        domainManager.registerDomain(DOMAIN_ID, {
+            major: 1,
+            minor: 0
+        });
+    }
+
+    domainManager.registerCommand(DOMAIN_ID, "start", start, true);
+    domainManager.registerCommand(DOMAIN_ID, "stop", stop, true);
+}
+
+function start(callback) {
+    setTimeout(callback.bind(null, null));
+}
+
+function stop(callback) {
+    setTimeout(callback.bind(null, null));
+}
+
+exports.init = init;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-define(function () {
+define(function (require, exports, module) {
     "use strict";
 
     // Dependencies
     var CommandManager = brackets.getModule("command/CommandManager");
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
     var Menus = brackets.getModule("command/Menus");
+    var NodeDomain = brackets.getModule("utils/NodeDomain");
 
     /**
      * @const
@@ -13,14 +15,28 @@ define(function () {
     var COMMAND_ID = "sbruchmann.staticpreview.toggleHTTPServer";
 
     /**
+     * @type {NodeDomain}
+     * @private
+     */
+    var httpServer = new NodeDomain(
+        "sbruchmann.staticpreview.HTTPServer",
+        ExtensionUtils.getModulePath(module, "HTTPServerDomain.js")
+    );
+
+    /**
      * @type {boolean}
      * @private
      */
     var isRunning = false;
 
     function toggleHTTPServer() {
-        isRunning = !isRunning;
-        CommandManager.get(COMMAND_ID).setChecked(isRunning);
+        httpServer.exec(!isRunning ? "start" : "stop")
+            .done(function callback() {
+               isRunning = !isRunning;
+            })
+            .always(function callback() {
+                CommandManager.get(COMMAND_ID).setChecked(isRunning);
+            });
     }
 
     CommandManager.register("Static Preview", COMMAND_ID, toggleHTTPServer);
